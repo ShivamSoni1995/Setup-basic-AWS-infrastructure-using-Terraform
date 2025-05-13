151e83538237658d0848cf053368eccd0dae193e
Shivam Soni <104499227+ShivamSoni1995@users.noreply.github.com>
2025-05-13 17:59:22 +0530

    defined 2 methods to write sg.tf

--- 3-security-group.tf
+++ 3-security-group.tf
@@ -1,3 +1,22 @@
+# Method 1: Creating a SG, creating rules separetly and associating with the rules with SG
+resource "aws_security_group" "sg_custom"{
+vpc_id = aws_vpc.custom_vpc.id
+tags = {
+    "Name" = "sg_custom"
+    }
+}
+
+resource "aws_security_group_rule" "egress" {
+    type = "egress"
+    from_port = 0
+    to_port = 0
+    protocol = -1
+    security_group_id = aws_security_group.sg_custom.id
+}
+
+------------------------------------------------------------------
+Method 2: Using ingress and egress arguments within the "aws_security_group" resource
+
 resource "aws_security_group" "sg_custom" {
     vpc_id = aws_vpc.custom_vpc.id
 
